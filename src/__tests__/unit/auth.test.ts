c9ac570c1318c31bc67d8993be1ebb6c46319747
fguzzo <fguzzo@edge-americas.com>
2022-07-22 16:12:02 -0300

    Try to resolver error in auth.test

--- src/__tests__/unit/auth.test.ts
+++ src/__tests__/unit/auth.test.ts
@@ -9,14 +9,17 @@ import * as UserService from "../../services/user.service";
 import * as AuthService from "../../services/auth.service";
 
 // TODO (a)
-// jest.mock("../services/auth.service", () => {
-//   const originalModule = jest.requireActual("../services/auth.service");
+// jest.mock("../../services/auth.service", () => {
+//   const originalModule = jest.requireActual("../../services/auth.service");
 //   return {
+//     __esModule: true,
 //     ...originalModule,
 //     createSession: jest.fn(() => sessionPayload),
 //     //createSession: () => Promise.resolve(sessionPayload),
 //   };
 // });
+
+const sessionInput = { email: "felipe.martin@example.com", password: "123456" };
 
 const userId = new mongoose.Types.ObjectId().toString();
 
@@ -24,93 +27,45 @@ const userPayload = {
   _id: userId,
   firstName: "Felipe",
   lastName: "Martin",
-  email: "felipe.martin@example.com",
+  email: sessionInput.email,
   verified: true,
 };
 
-// TODO: es correcto definir asi los metodos de UserModel
-const userDocument = {
-  ...userPayload,
-  toJSON: () => {
-    return { ...userPayload };
-  },
-  comparePassword: () => true,
-};
-
 const sessionPayload = {
-  _id: new mongoose.Types.ObjectId().toString(),
+  _id: new mongoose.Types.ObjectId(),
   user: userId,
   valid: true,
   userAgent: "PostmanRuntime/7.28.4",
-  createdAt: new Date("2021-09-30T13:31:07.674Z"),
-  updatedAt: new Date("2021-09-30T13:31:07.674Z"),
-  __v: 0,
+  // createdAt: new Date("2021-09-30T13:31:07.674Z"),
+  // updatedAt: new Date("2021-09-30T13:31:07.674Z"),
+  // __v: 0,
 };
 
-describe("create user session", () => {
+describe.skip("create user session", () => {
   describe("given the username and password are valid", () => {
-    /*
     it("should return a signed accessToken & refresh token", async () => {
-      jest
-        .spyOn(UserService, "findUserByEmail")
-        // @ts-ignore
-        .mockReturnValue(userDocument);
+      // @ts-ignore
+      jest.spyOn(UserService, "findUserByEmail").mockReturnValue(userPayload);
+
+      // @ts-ignore
+      jest.spyOn(UserService, "validatePassword").mockReturnValue(true);
+
       jest
         .spyOn(AuthService, "createSession")
         // @ts-ignore
         .mockReturnValue(sessionPayload);
 
-      const req = {
-        get: () => {
-          return "a user agent";
-        },
-        body: {
-          email: "felipe.martin@example.com",
-          password: "Password123",
-        },
-      };
+      // console.log(
+      //   "MOCK",
+      //   createSession({ userId: "a", userAgent: "b" })
+      // );
 
-      const send = jest.fn();
-
-      const res = {
-        send,
-      };
-
-      const next = jest.fn();
-
-      // @ts-ignore
-      await createSessionHandler(req, res, next);
-
-      expect(send).toHaveBeenCalledWith({
-        accessToken: expect.any(String),
-        refreshToken: expect.any(String),
-      });
-    });
-    */
-
-    it.skip("should return a signed accessToken & refresh token", async () => {
-      const userServiceSpy = jest.spyOn(UserService, "findUserByEmail");
-      // @ts-ignore
-      userServiceSpy.mockReturnValue(userDocument);
-
-      // TODO (a)
-      // jest
-      //   .spyOn(AuthService, "createSession")
-      //   // @ts-ignore
-      //   .mockReturnValue(sessionPayload);
-
-      const createSessionSpy = jest.spyOn(AuthService, "createSession");
-      createSessionSpy.mockImplementationOnce(() => {
-        throw new Error("Invalid access token");
-      });
-
-      const { statusCode, body } = await supertest(app)
+      const response = await supertest(app)
         .post("/api/v1/sessions")
-        .send({
-          email: "felipe.martin@example.com",
-          password: "Password123",
-        });
-      expect(statusCode).toBe(200);
+        .send(sessionInput)
+        .expect(200);
+
+      expect(response.body).not.toBeNull();
     });
   });
 });
